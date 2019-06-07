7650acdcb1b478ffac285efe78883c64a02f24f0
Pellumb Baboci <pellumbbaboci@gmail.com>
2019-06-07 17:56:35 +0300

    Add files via upload

---
+++ CinsWidgetClientConsole/src/Main.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try{
+            //Initialize socket
+            Socket socket = new Socket(InetAddress.getByName("localhost"), 5000);
+            byte[] contents = new byte[10000];
+            String fileFolder = System.getProperty("user.dir")+ "\\src\\downloads\\";
+
+            //Initialize the FileOutputStream to the output file's full path.
+
+
+            InputStream is = socket.getInputStream();
+
+
+            DataOutputStream outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+            DataInputStream inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+
+            outputStream.writeUTF("filenames");
+            outputStream.flush();
+            System.out.println("sent");
+
+            String fileNamesConcat = inputStream.readUTF();
+            System.out.println(fileNamesConcat);
+
+            String[] fileNameList = fileNamesConcat.split(",");
+
+
+            outputStream.writeUTF("download,"+ fileNameList[1]);
+            outputStream.flush();
+            System.out.println("download request sent");
+
+            FileOutputStream fos = new FileOutputStream(fileFolder+fileNameList[1]);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            //No of bytes read in one read() call
+            int bytesRead = 0;
+
+            while((bytesRead=is.read(contents))!=-1){
+                System.out.println(bytesRead);
+                bos.write(contents, 0, bytesRead);
+            }
+
+
+            System.out.println("bisey23");
+
+
+
+            bos.flush();
+
+//
+            System.out.println("File saved successfully!");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println("File saved successfully2!");
+
+    }
+}
---
+++ CinsWidgetServer/src/Main.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static List<ServerThread> serverThreadList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        String folderName = System.getProperty("user.dir")+ "\\src\\resources\\";
+        //Initialize Sockets
+        ServerSocket ssock = null;
+        try {
+            ssock = new ServerSocket(5000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileScanThread fileScanThread = new FileScanThread(new File(folderName));
+        Thread thread2 = new Thread(fileScanThread);
+        thread2.start();
+
+
+        while(true){
+            try{
+                Socket socket = ssock.accept();
+                System.out.println("Socket accepted.");
+                ServerThread serverThread = new ServerThread(socket, folderName);
+                serverThreadList.add(serverThread);
+                Thread thread = new Thread(serverThread);
+                thread.start();
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
